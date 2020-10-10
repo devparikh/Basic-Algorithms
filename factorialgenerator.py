e44d699be8ee315cd51b16110d92f72e42638108
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2020-10-10 10:58:02 -0400

    Update factorialgenerator.py

--- factorialgenerator.py
+++ factorialgenerator.py
@@ -2,11 +2,18 @@ def getfactorial(x):
     factorial_input = int(input("What factorial do you want to calculate?:"))
     factorial = 1 
      # Calculating the factorial
-    if factorial_input <= 1:
-      print("This is not valid!")
+    if factorial_input == 1:
+      print("The value is 1")
     elif factorial_input > 0:
       for i in range(1, factorial_input+1):
         factorial= factorial * i 
       print("The factorial of {n} is {f}".format(n=factorial_input, f=factorial))
+    if factorial_input == 0:
+        print("The factorial of 0 is 1!")
+    elif factorial_input < 0:
+        print("Invalid Input! Please try another factorial")
+        factorial_input()
+        
+        
 getfactorial('x')
   
