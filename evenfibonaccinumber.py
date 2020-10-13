b77ba698d8c4d432932fac6b3120200aea4e3c13
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2020-10-13 19:02:18 -0400

    Update evenfibonaccinumber.py

--- evenfibonaccinumber.py
+++ evenfibonaccinumber.py
@@ -1,19 +1,13 @@
 # Here I am creating the 3 most important variables to create the fibonacci sequence
-before_previous_num = 1
-previous_num = 1
-n = 0
-fib_series = 0
+number = [0,1]
+fib_series = []
 # Here I am having a while loop to make sure that the number n is less then 4000000
-while n < 4000000:
+while n <= 4000000:
     n = before_previous_num + previous_num
     # Here its checking if n is even and if it is even then its adding the value to the list and printing the sum of that list
     if n % 2 == 0:
         fib_series += n
-        
-    # Here we are changing the values to make sure that they are changing every time the loop starts again 
-    previous_num = before_previous_num
-    n = previous_num
-print(fib_series)
+    print(sum(fib_series))
          
 
 
